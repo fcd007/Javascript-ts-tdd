6d19ae1ed6b803c6378dc559ca56a1b833a5dc05
Claudeilton Dantas <fcd007@hotmail.com>
2021-03-25 08:59:27 -0300

    feat: init project JS essential work

---
+++ 01-conceitos-js/exercicios-01.js
@@ -0,0 +1,7 @@
+const nome = 'Francisco Claudeilton'
+const sobrenome = 'Dantas'
+let age = 31
+let weight = 63
+let height = 1.6
+let IMC = weight / Math.pow(height, 2)
+console.log(`${nome} ${sobrenome} tem ${age}, pesa ${weight}kg \ntem ${height} de altura e seu IMC é ${IMC}`)
---
+++ 01-conceitos-js/exercicios-02.js
@@ -0,0 +1,11 @@
+let varA = 'A'
+let varB = 'B'
+let varC = 'C'
+let swap = ''
+console.log(varA, varB, varC)
+//fazendo as trocas
+swap = varA // swap = A - guarda e protege
+varA = varB // varA = B
+varB = varC // varB = C
+varC = swap // varC = A
+console.log(varA, varB, varC)
