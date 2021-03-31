64332715ff0abef4436407a338fccf254f8523a6
Claudeilton Dantas <fcd007@hotmail.com>
2021-03-30 21:04:54 -0300

    feat: ensure let and var diference context

---
+++ 02-algoritmos-js/samples/07-let-var.js
@@ -0,0 +1,33 @@
+const verdade = true;
+// //let tem escopo de bloco então é local
+// //var tem escopo e função
+
+// let nome = 'Dantas';
+// console.log(nome);
+// var nome2 = 'Claudeilton';
+// if (verdade) {
+//   let nome = 'Francisco'
+//   console.log(nome);
+//   var nome2 ='OutroNone'
+// }
+
+// var sobrenome = 'NomeAtual';
+// function mostraNome() {
+//   var sobrenome = 'Lucas Miranda';
+//   console.log(sobrenome);
+// }
+
+// mostraNome();
+
+// function falaOi() {
+//   var texto = 'Claudeilton';
+//   if(verdade) {
+//     let nome = 'Dantas';
+//     console.log(texto, nome);
+//   }
+// }
+
+// falaOi()
+
+console.log(nome); // resultado não definida
+let nome = 'Dantas'; //elevação de variável
