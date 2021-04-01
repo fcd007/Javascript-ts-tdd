88c9d8209600ea5e35f2fb6f8cf4c1ea32fbd364
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-01 20:59:30 -0300

    feat: error try-catch functions

---
+++ 05-lista-praticas/04-erros-js.js
@@ -0,0 +1,20 @@
+// try {
+//   console.log(erro);
+// } catch (error) {
+//     console.log('Criar variável ao início do código!');
+//     console.log(error);
+// }
+function soma(numero, numero2) {
+  if (typeof numero !== "number" || typeof numero2 !== "number") {
+    throw new Error("Numero ou numero2 precisam ser números");
+  }
+
+  return numero + numero2;
+}
+
+try {
+  console.log(soma(2, 4));
+  console.log(soma(1, "o"));
+} catch (error) {
+  console.log(error);
+}
