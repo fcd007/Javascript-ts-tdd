e5b12659b563e8553e7c658406745c3767ed20cc
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-02 17:51:38 -0300

    refactor: change addEventLIsterner values element add document event

--- 08-relogio-pratica/model/asset/js/index.js
+++ 08-relogio-pratica/model/asset/js/index.js
@@ -12,21 +12,43 @@ let segundosTempo = 0;
 let timerPausa;
 
 function iniciarTempo() {
-  timer = setInterval(() => {
+  timerPausa = setInterval(() => {
     segundosTempo++;
     relogio.innerHTML = criarHoraSegundos(segundosTempo);
   }, 1000);
 }
-iniciar.addEventListener("click", function (event) {
-  // relogio.innerHTML = tempo.toLocaleTimeString("pt-BR", { hour12: false });
-  iniciarTempo();
+//criar captura do evento de forma global para poder selecionar o clique
+document.addEventListener('click', (event) => {
+  const element = event.target;
+
+  if (element.classList.contains("iniciar")) {
+    relogio.classList.remove("pausado");
+    clearInterval(timerPausa);
+    iniciarTempo();
+  } else if (element.classList.contains("pausar")) {
+    relogio.classList.add("pausado");
+    clearInterval(timerPausa);
+  } else if (element.classList.contains("zerar")) {
+    clearInterval(timerPausa);
+    relogio.innerHTML = "00:00:00";
+    segundosTempo = 0;
+  }
 });
 
-pausar.addEventListener("click", function (event) {
-  clearInterval(timer);
-});
+// iniciar.addEventListener("click", function (event) {
+//   // relogio.innerHTML = tempo.toLocaleTimeString("pt-BR", { hour12: false });
+//   relogio.classList.remove("pausado");
+//   clearInterval(timerPausa);
+//   iniciarTempo();
+// });
 
-zerar.addEventListener("click", function (event) {
-  clearInterval(timer);
-  relogio.innerHTML = "00:00:00";
-});
+// pausar.addEventListener("click", function (event) {
+//   relogio.classList.add("pausado");
+//   clearInterval(timerPausa);
+// });
+
+// zerar.addEventListener("click", function (event) {
+//   clearInterval(timerPausa);
+//   relogio.innerHTML = "00:00:00";
+//   segundosTempo = 0;
+// });
