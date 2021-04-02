854bc72a816c80bf5f78964caba0105189e3a5c0
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-02 20:40:52 -0300

    feat: init project todo task list js with localstorage

--- 09-todo-lista-tarefas/model/asset/js/index.js
+++ 09-todo-lista-tarefas/model/asset/js/index.js
@@ -1,1 +1,64 @@
-const input = document.querySelector(".input-terefa");
+const inputTarefa = document.querySelector(".input-tarefa");
+const btnTarefa = document.querySelector(".btn-tarefa");
+const tarefas = document.querySelector(".tarefas");
+
+function salvarTarefas() {
+  const listaTarefas = tarefas.querySelectorAll("li");
+  const listaTarefasSalva = [];
+
+  for (let lista of listaTarefas) {
+    let tarefaTexto = lista.innerText;
+    tarefaTexto = tarefaTexto.replace("apagar", "").trim();
+    listaTarefasSalva.push(tarefaTexto);
+  }
+
+  const tarefasJSON = JSON.stringify(listaTarefasSalva);
+  localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function limpaInput() {
+  inputTarefa.value = "";
+  inputTarefa.focus();
+}
+
+function criaLista() {
+  const li = document.createElement("li");
+  return li;
+}
+
+function criaBotaoApagar(lista) {
+  lista.innerHTML += " ";
+  const botaoApagar = document.createElement("button");
+  botaoApagar.innerHTML = "apagar";
+  botaoApagar.setAttribute("class", "apagar");
+  lista.appendChild(botaoApagar);
+}
+
+function criaTarefa(textoInput) {
+  const lista = criaLista();
+  lista.innerHTML = textoInput;
+  tarefas.appendChild(lista);
+  limpaInput();
+  criaBotaoApagar(lista);
+  salvarTarefas();
+}
+
+inputTarefa.addEventListener("keypress", function (event) {
+  if (event.keyCode === 13) {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+  }
+});
+
+btnTarefa.addEventListener("click", function (event) {
+  if (!inputTarefa.value) return;
+  criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener("click", function (event) {
+  const element = event.target;
+  if (element.classList.contains("apagar")) {
+    element.parentElement.remove();
+    salvarTarefas();
+  }
+});
