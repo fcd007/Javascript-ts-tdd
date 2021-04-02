3d32add6b413734e80974685109cb5a19a2926f7
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-01 21:41:41 -0300

    feat: error try-catch-finaly tests

--- 06-erros-parte2/01-try-catch-error.js
+++ 06-erros-parte2/01-try-catch-error.js
@@ -1,11 +1,35 @@
+// try {
+//   // é executado quando não ocorre erros ( não há erros)
+//   console.log("Abrir um socket");
+//   console.log("Manipular operação");
+// } catch (error) {
+//   //é executado quando ocorre erros
+//   console.log("Tratar o erro que ocorreu!");
+// } finally {
+//   //sempre executa
+//   console.log("Fechar o socket");
+// }
+
+function mostraHora(data) {
+  if (data && !(data instanceof Date)) {
+    throw new TypeError("Esperando uma instância de hora!");
+  }
+
+  if (!data) {
+    data = new Date();
+  }
+
+  return data.toLocaleTimeString("pt-BR", {});
+}
+
 try {
-  // é executado quando não ocorre erros ( não há erros)
-  console.log('Abrir um socket');
-  console.log('Manipular operação');
+  const dataAtual = new Date("01-01-1970 12:58:12");
+  const hora = mostraHora(11);
+  // const hora = mostraHora(dataAtual);
+  console.log(hora);
 } catch (error) {
-  //é executado quando ocorre erros
-  console.log('Tratar o erro que ocorreu!');
-} finally {
-  //sempre executa
-  console.log('Fechar o socket');
+  //executa quando dá erro - tratar erro
+  console.log(error);
+}finally {
+  console.log("Sempre executa, bora codar melhor!");
 }
