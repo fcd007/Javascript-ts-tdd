8b3092d2ec936c24050991055d9b25083f653747
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-02 10:22:16 -0300

    feat: use setTimeInterval and setTimeOut Date object

---
+++ 07-times-tempo/01-time-setInterval.js
@@ -0,0 +1,21 @@
+function mostraHora() {
+  let data = new Date();
+
+  return data.toLocaleTimeString("pt-BR", { hour12: false });
+}
+
+function funcaoDoInterval() {
+  console.log(mostraHora());
+}
+//usando uma função anônima
+const timer = setInterval(function () {
+  console.log(mostraHora());
+}, 1000);
+
+setTimeout(function () {
+  clearInterval(timer);
+}, 5000);
+
+setTimeout(function () {
+  console.log('Olá mundo');
+}, 3000);
