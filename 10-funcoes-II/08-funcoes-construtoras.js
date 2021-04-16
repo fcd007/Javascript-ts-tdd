d237af81745c775c1f308d1bfb32994701d0367a
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-16 12:10:29 -0300

    feat: functions construtors add arrow functions

--- 10-funcoes-II/08-funcoes-construtoras.js
+++ 10-funcoes-II/08-funcoes-construtoras.js
@@ -3,17 +3,32 @@
 //construtoras -> nomeclatura - Maiúscula inicial
 // Pessoa (new)
 function Pessoa(nome, sobrenome) {
+  const id = 1234567978; //variables privates
+
+  const metodoInterno = function () {
+    
+  }
+
   this.nome = nome;
   this.sobrenome = sobrenome;
 
-  //criando 
+  this.meuMetodo1 = function() {
+    //usando função padrão
+    console.log(this.nome + ": método com função");
+  };
+  this.meuMetodo2 = () => {
+    //usando arrow function
+    console.log(this.nome + ": método com arrow function");
+  };
 }
 
 const pessoa1 = new Pessoa("Claudeilton", "Dantas");
-const pessoa2 = new Pessoa("Felipe", "Santos");
+pessoa1.meuMetodo1();
+pessoa1.meuMetodo2();
+// const pessoa2 = new Pessoa("Felipe", "Santos");
 
-console.log(typeof(pessoa1));
-console.log(typeof(pessoa2));
+// console.log(typeof pessoa1);
+// console.log(typeof pessoa2);
 
-console.log(pessoa1);
-console.log(pessoa2);
+// console.log(pessoa1);
+// console.log(pessoa2);
