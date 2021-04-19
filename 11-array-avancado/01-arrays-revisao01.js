2c08103b6caa5137d961e2e791dc3de098280b46
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-19 16:03:47 -0300

    feat: test case use Array object

---
+++ 11-array-avancado/01-arrays-revisao01.js
@@ -0,0 +1,31 @@
+// const nomes = ["Eduardo", "Maria", "Giuliana"];
+// nomes[2] = "Lucas";
+// delete nomes[2];
+// console.log(nomes);
+//Strings, objetos, funções, números
+// const nomes = new Array("Felipe", "Juana", "Lucas");
+//Arrays são valor por referência
+// const novo = nomes;
+//removendo por referência
+// const valor = novo.pop();
+// console.log('removeu ', valor, nomes);
+// const novoArray = [...nomes];
+// console.log(novoArray);
+// console.log(novoArray.length); //tamanho
+//removendo
+// const removidoInicio = nomes.shift();
+// console.log(nomes);
+// const insereInicio = nomes.unshift("Luana"); //insere no início
+// console.log(nomes);
+
+// const insereFinal = nomes.push("João"); //insere no final do array
+// console.log(nomes);
+// const nomes = ["Eduardo", "Lucas", "Maria", "Fernanda"];
+// const novoNomes = nomes.slice(1,-1);
+// console.log(novoNomes);
+// const nome = 'Francisco Claudeilton Dantas';
+// const nomes = nome.split(' '); //quebra uma strign em array separado por espaço
+const nomes = ["Francisco", "Claudeilton", "Dantas"];
+console.log(nomes);
+const nome = nomes.join(' ');
+console.log(nome);
