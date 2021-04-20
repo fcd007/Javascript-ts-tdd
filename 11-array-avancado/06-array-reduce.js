1fdbc9042ec8d574d5ca18ef763a1b77ef6a282a
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-20 18:34:25 -0300

    feat: add test reduce with objects

--- 11-array-avancado/06-array-reduce.js
+++ 11-array-avancado/06-array-reduce.js
@@ -1,29 +1,35 @@
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-// some todos os números (reduce)
-const total = numeros.reduce((acumulador, valor) => {
-  return (acumulador += valor);
-});
+// // some todos os números (reduce)
+// const total = numeros.reduce((acumulador, valor) => {
+//   return (acumulador += valor);
+// });
 
-// retorne um array com os pares (filter)
-const pares = numeros.reduce((acumulador, valor) => {
-  if (valor % 2 === 0) acumulador.push(valor);
-  return acumulador;
-}, []);
-console.log(pares);
+// // retorne um array com os pares (filter)
+// const pares = numeros.reduce((acumulador, valor) => {
+//   if (valor % 2 === 0) acumulador.push(valor);
+//   return acumulador;
+// }, []);
+// console.log(pares);
 
-// retorne um array com o dobro dos valores(Map)
-const dobro = numeros.reduce((acumulador, valor) => {
-  acumulador.push(valor * 2);
-  return acumulador;
-}, []);
-console.log(dobro);
+// // retorne um array com o dobro dos valores(Map)
+// const dobro = numeros.reduce((acumulador, valor) => {
+//   acumulador.push(valor * 2);
+//   return acumulador;
+// }, []);
+// console.log(dobro);
 
 const pessoas = [
-  { nome: "Dantas", idade: 31 },
   { nome: "Luiz", idade: 19 },
   { nome: "Diana", idade: 22 },
   { nome: "Duda", idade: 30 },
-  { nome: "Lucas", idade: 51 },
-  { nome: "Maria", idade: 53 },
+  { nome: "Lucas", idade: 59 },
+  { nome: "Dantas", idade: 31 },
+  { nome: "Luana", idade: 53 },
 ];
+
+const maisIdade = pessoas.reduce((acumulador, valor) => {
+  if (acumulador.idade >= valor.idade) return acumulador;
+  return valor;
+});
+console.log(maisIdade);
