f7521331477fea30a2c562ec0a1b33add81c39dd
Claudeilton Dantas <fcd007@hotmail.com>
2021-04-20 17:15:12 -0300

    feat: add test map return arrays

---
+++ 11-array-avancado/04-array-map.js
@@ -0,0 +1,20 @@
+//retorna um novo array modificado
+// const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+// //dobrar os valores dos números
+// const numerosDobro = numeros.map((valor) => {
+//   return valor * 2;
+// });
+
+const nomes = [
+  { nome: "Dantas", idade: 31 },
+  { nome: "Luiz", idade: 19 },
+  { nome: "Diana", idade: 22 },
+  { nome: "Duda", idade: 30 },
+  { nome: "Lucas", idade: 51 },
+  { nome: "Maria", idade: 53 },
+];
+// console.log(numerosDobro);
+//para cada elemento
+//retorne apenas uma string com o nome da pessoa
+//remova apenas a chave "nome" do objeto
+//adicione uma chave id em casa objeto
